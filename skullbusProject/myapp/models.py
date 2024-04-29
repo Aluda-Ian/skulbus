9d421c5c1b7233c46c798c48eb08ccbd444ece4c
Ian Aluda <ianaluda27@students.uonbi.ac.ke>
2024-04-29 16:47:16 +0300

    Update models.py

--- skullbusProject/myapp/models.py
+++ skullbusProject/myapp/models.py
@@ -8,9 +8,9 @@ class Bus(models.Model):
     bus_name = models.CharField(max_length=30)
     source = models.CharField(max_length=30)
     dest = models.CharField(max_length=30)
-    nos = models.DecimalField(decimal_places=0, max_digits=2)
-    rem = models.DecimalField(decimal_places=0, max_digits=2)
-    price = models.DecimalField(decimal_places=2, max_digits=6)
+    nos = models.DecimalField(decimal_places=0, max_digits=30)
+    rem = models.DecimalField(decimal_places=0, max_digits=30)
+    price = models.DecimalField(decimal_places=2, max_digits=30)
     date = models.DateField()
     time = models.TimeField()
 
